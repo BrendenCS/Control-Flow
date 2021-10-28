e91395a00621b652bd2e751a50a7e56b6d5c0135
Brenden Krueger <bkruegercomputerscience@gmail.com>
2021-10-28 12:55:34 -0400

    Added ELIF Statement for a Deposit

--- Control Flow.py
+++ Control Flow.py
@@ -38,13 +38,16 @@ if ATM == "yes":
         print("\nYour Balance: $" + str(balance))
 
         # Ask users what type of transaction they want - Withdrawal - Deposit
-        typeOfTransaction = input("\nWould you like to make a Withdrawl or a Deposit\nW = Withdrawal or D = Deposit: ").lower()
+        typeOfTransaction = input("\nWould you like to make a Withdrawal, Deposit, or check your Balance\nW = Withdrawal or D = Deposit or B = Balance: ").lower()
         if typeOfTransaction == "w":
-            withdrawlAmount = int(input("Enter amount of withdrawl: "))
-            balance = balance - withdrawlAmount
+            withdrawalAmount = int(input("Enter amount of withdrawal: "))
+            balance = balance - withdrawalAmount
             print("Your new balance is: $" + str(balance))
 
-            
+         elif typeOfTransaction == "d":
+             depositAmount = int(input("Enter amount of your deposit: "))
+             balance = balance + depositAmount
+             print("Your new balance is: $" + str(balance))
 
 
     else:
